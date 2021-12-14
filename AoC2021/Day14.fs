080eb752103be2a920c3a05c496422130cf83313
Jeff Williams <jeffw@wherethebitsroam.com>
2021-12-14 14:46:38 +0100

    more cleanup

--- AoC2021/Day14.fs
+++ AoC2021/Day14.fs
@@ -31,17 +31,18 @@ let parse (s: string) =
         |> Map.ofSeq
 
     // group input by pairs and count each pair
-    let counts =
+    let pairs =
         chars
         |> List.pairwise
         |> List.map (fun pair -> (pair, 1L))
         |> groupSum
 
-    (h, t, counts, m)
+    (h, t, pairs, m)
 
+// map a pair to it's expansion, keeping the same count
 let expand m (pair, count) =
-    let expansion = Map.find pair m
-    expansion |> List.map (fun pair -> (pair, count))
+    Map.find pair m
+    |> List.map (fun pair -> (pair, count))
 
 // map each pair to their expansion, keeping the count, they re-group
 let step m pairs =
@@ -50,16 +51,12 @@ let step m pairs =
 // since all characters are pairwise, we will have double
 // of all chars except for the first and last
 let counts (h: char) (t: char) (pairs: list<(char * char) * int64>) =
-    let count =
-        pairs
-        |> List.collect (fun (pair, count) -> [ (fst pair, count); (snd pair, count) ])
-
-    // add an extra copy of the head and tail so everything is double
+    // add an extra copy of the first and last so everything is double
     // then divide the count by 2
-    count @ [ (h, 1); (t, 1) ]
+    ((h, t), 1L) :: pairs
+    |> List.collect (fun (pair, count) -> [ (fst pair, count); (snd pair, count) ])
     |> groupSum
     |> List.map (fun (x, count) -> (x, count / 2L))
-
 
 let part1 (s: string) =
     let (h, t, pairs, map) = parse s
