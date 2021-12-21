c2a6b266715e56db89421bffb9b0fc427a35a96d
Jeff Williams <jeffw@wherethebitsroam.com>
2021-12-21 10:41:13 +0100

    Day21

---
+++ AoC2021/Day21.fs
@@ -0,0 +1,74 @@
+module Day21
+
+type Dice() =
+    let mutable next = 0
+    let mutable count = 0
+
+    member _.Roll =
+        count <- count + 1
+        next <- (next % 100) + 1
+        next
+
+    member _.Count = count
+
+type Player = { pos: int; score: int }
+
+let rec play (dice: Dice) (p1: Player) (p2: Player) =
+    let rolls =
+        [ dice.Roll; dice.Roll; dice.Roll ] |> List.sum
+
+    let pos = (p1.pos + rolls - 1) % 10 + 1
+    let score = p1.score + pos
+    let p1 = { pos = pos; score = score }
+
+    if score >= 1000 then
+        (p1, p2)
+    else
+        play dice p2 p1
+
+let part1 pos1 pos2 =
+    let p1 = { pos = pos1; score = 0 }
+    let p2 = { pos = pos2; score = 0 }
+    let dice = Dice()
+
+    let (_, loser) = play dice p1 p2
+
+    loser.score * dice.Count
+
+let possible =
+    [ 1 .. 3 ]
+    |> List.collect (fun r1 ->
+        [ 1 .. 3 ]
+        |> List.collect (fun r2 ->
+            [ 1 .. 3 ]
+            |> List.collect (fun r3 -> [ r1 + r2 + r3 ])))
+    |> List.countBy id
+    |> List.map (fun (r, c) -> (r, int64 c))
+
+let folder (m: Map<int, int64>) (p, c) =
+    m
+    |> Map.change p (fun x ->
+        match x with
+        | Some x -> Some(x + c)
+        | None -> Some(c))
+
+let rec play2 (player: int) (chances: int64) (p1: Player) (p2: Player) =
+    possible
+    |> List.collect (fun (roll, count) ->
+        let pos = (p1.pos + roll - 1) % 10 + 1
+        let score = p1.score + pos
+        let p1 = { pos = pos; score = score }
+
+        if score >= 21 then
+            [ (player, chances * count) ]
+        else
+            let player = if player = 1 then 2 else 1
+            play2 player (chances * count) p2 p1)
+    |> List.fold folder (Map [])
+    |> Map.toList
+
+let part2 pos1 pos2 =
+    let p1 = { pos = pos1; score = 0 }
+    let p2 = { pos = pos2; score = 0 }
+
+    play2 1 1 p1 p2
--- AoC2021/Program.fs
+++ AoC2021/Program.fs
@@ -4,4 +4,12 @@ let data =
 let test =
     System.IO.File.ReadAllText @"../../day19-test.txt"
 
-printfn "%A" (Day19.part2 data)
+// real
+// Player 1 starting position: 3
+// Player 2 starting position: 5
+
+// test
+// Player 1 starting position: 4
+// Player 2 starting position: 8
+
+printfn "%A" (Day21.part2 3 5)
